a8d7bc39b873fea711935dc1b671a22226d5f026
Александр Васянькин <henchfiend@yandex.ru>
2024-10-14 18:51:20 +0300

    Модальное окно и иконки
    - загружены все иконки для модельного окна
    - создан каркас модального окна

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -8,6 +8,14 @@ function App() {
     <div className={styles.app}>
       <Header />
       <Main />
+      <div className={styles.modal}>
+        <button className={styles[`close-button`]}>
+          <div className={styles[`close-icon`]}></div>
+        </button>
+        <div>первый инпут</div>
+        <div>второй инпут</div>
+        <div>третий инпут</div>
+      </div>
     </div>
   )
 }
