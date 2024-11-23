81c862aadacd053bcdd9296c504752d58d68fe56
Александр Васянькин <henchfiend@yandex.ru>
2024-11-23 16:13:39 +0300

    Анимации картин
    - Анимации в компоненте picture приведены к макету

--- src/components/picture/picture.tsx
+++ src/components/picture/picture.tsx
@@ -10,11 +10,18 @@ export const Picture = ({ item }: TPictureItem) => {
         src={BASE_URL + item.imageUrl}
         alt={item.name}
       />
-      <div className={styles[`discription-container`]}>
+      <div className={styles[`white-box`]}>
+        <div className={styles[`red-column`]}></div>
+      </div>
+      <div
+        className={`${styles[`discription-container`]} ${styles[`move-down`]}`}
+      >
         <h2 className={styles.name}>{item.name}</h2>
         <p className={styles.date}>{item.created}</p>
       </div>
-      <div className={styles[`discription-container`]}>
+      <div
+        className={`${styles[`discription-container`]} ${styles[`move-right`]}`}
+      >
         <h2 className={styles.name}>{item.authorId}</h2>
         <p className={styles.date}>{item.locationId}</p>
       </div>
