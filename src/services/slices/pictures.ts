dc4e12afa4a2f3f95da7df3c3f103ce2c2037cfa
Александр Васянькин <henchfiend@yandex.ru>
2024-12-06 00:40:30 +0300

    store для pictures

---
+++ src/services/slices/pictures.ts
@@ -0,0 +1,23 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { TPicture } from "../../components/types/types";
+
+type TPicturesState = {
+    pictures: TPicture[]
+}
+
+const initialState: TPicturesState = {
+    pictures: []
+}
+
+const picturesSlice = createSlice({
+    name: 'pictures',
+    initialState,
+    reducers: {
+        getPictures: (state, action) => {
+            state.pictures = action.payload
+        },
+    }
+})
+
+export const { getPictures } = picturesSlice.actions;
+export const picturesReducer = picturesSlice.reducer;
--- src/services/store/store.ts
+++ src/services/store/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { api } from "../../api/api";
 import { modalReducer } from "../slices/modal";
+import { picturesReducer } from "../slices/pictures";
 
 export const store = configureStore({
   reducer: {
     [api.reducerPath]: api.reducer,
     modal: modalReducer,
+    pictures: picturesReducer,
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
