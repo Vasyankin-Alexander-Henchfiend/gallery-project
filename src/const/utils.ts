ef916c37f0b1655d6d3f5be7423c2b7125edcde9
Александр Васянькин <henchfiend@yandex.ru>
2024-12-07 02:02:39 +0300

    фикс ошибки отрисовки Пагинатора

--- src/const/utils.ts
+++ src/const/utils.ts
@@ -3,6 +3,12 @@ export const getTotalPageCount = (rowCount: number, limit: number): number =>
   Math.ceil(rowCount / limit);
 
 // Функция для получения нового однотипного массива
-export function getPureArray<T>(array: T[] | undefined, value: keyof T, id: keyof T) {
-  return array?.map((item) => {return { id: +item[id], label: item[value]}});
+export function getPureArray<T>(
+  array: T[] | undefined,
+  value: keyof T,
+  id: keyof T
+) {
+  return array?.map((item) => {
+    return { id: +item[id], label: item[value] };
+  });
 }
