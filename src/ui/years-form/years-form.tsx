c4b96199fa9a21c897560cb325d156815d15d1ef
Александр Васянькин <henchfiend@yandex.ru>
2025-02-06 00:16:06 +0300

    Input в YearsForm
    - компонент Input теперь добавлен в YearsForm
    - компонент YearsForm теперь отвечает требованиям макета при смене темы

--- src/ui/years-form/years-form.tsx
+++ src/ui/years-form/years-form.tsx
@@ -1,17 +1,18 @@
+import { Input } from '../input/input'
 import styles from './years-form.module.scss'
 
 export const YearsForm = () => {
   return (
     <form className={styles.form}>
-      <input
-        className={styles.input}
+      <Input
+        extraClass={styles.input}
         placeholder="From"
         maxLength={4}
         type="text"
       />
       <div className={styles.icon}></div>
-      <input
-        className={styles.input}
+      <Input
+        extraClass={styles.input}
         placeholder="To"
         maxLength={4}
         type="text"
